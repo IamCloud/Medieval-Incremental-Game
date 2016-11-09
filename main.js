09067cc2726d74bf6802c8f2eb8e26adf0982900
Nicholas Thérien Roussel <NicolasNtr11@gmail.com>
2016-11-08 19:39:11 -0500

    Added Currency!
    Added Gold. Changed gameplay. Lifebar now refreshes every 1/10 second.

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-var charName= "Pemda";
+var charName = "Pemda";
 var EXP = 0.0;
+var Currency = 0.0;
+
 var WeaponCost = 10;
-var WeaponBonus = 1;
-var WeaponQuantity = 0;
-var WeaponTotalBonus = 0;
+var WeaponDamage = 1;
+var WeaponLevel = 0;
+var WeaponName = "Unarmed";
+
+var WeaponNames = ["Unarmed", "Gauntlet", "Spiked Gauntlet", "Dagger", 
+"Light mace", "Light hammer", "Sickle", "Club", "Handaxe", "Shortspear", 
+"Longspear", "Heavy mace", "Battleaxe", "Flail", "Longsword", "Trident", "Warhammer",
+"Glaive", "Greatclub", 
+"Greataxe", "Greatsword", "God Weapon"];
+
 var DummyCost = 100;
 var DummyBonus = 5;
 var DummyQuantity = 0;
@@ -13,18 +22,22 @@ var ConcentrationBonus = 10;
 var ConcentrationQuantity = 0;
 var ConcentrationTotalBonus = 0;
 
-var begCost = 100;
+var begCost = 50;
 var begBonus = 1;
 var begQuantity = 0;
 var begTotalBonus = 0;
-var advCost = 1000;
+var advCost = 500;
 var advBonus = 5;
 var advQuantity = 0;
 var advTotalBonus = 0;
-var senCost = 1000;
-var senBonus = 5;
+var senCost = 5000;
+var senBonus = 20;
 var senQuantity = 0;
 var senTotalBonus = 0;
+var godCost = 100000;
+var godBonus = 100;
+var godQuantity = 0;
+var godTotalBonus = 0;
 
 var expPclick = 0.0;
 var hitPsec = 0;
@@ -33,6 +46,8 @@ var TurnDamage = 0.0;
 
 var totalLife = 10;
 var life = 10;
+
+var refreshTime = 100;
 
 
 function DummyMouseUp(){		
@@ -45,100 +60,89 @@ function getCharName(){
 }
 
 function addWeapon(){
-	if (EXP >= WeaponCost){
-		EXP -= WeaponCost;
-		WeaponQuantity++;
-		WeaponCost = Math.round(WeaponCost * (1.15));
-		WeaponTotalBonus++;
+	if (Currency >= WeaponCost){
+		Currency -= WeaponCost;
+		WeaponCost = Math.round(WeaponCost * (1.2));
+		WeaponDamage = Math.round(WeaponDamage * (1.15) + 1);
+		WeaponLevel++;
+		if (WeaponNames.length > WeaponLevel) {
+			WeaponName = WeaponNames[WeaponLevel];
+		}
 		document.getElementById('WeaponCost').innerHTML = WeaponCost;
-		document.getElementById('WeaponQuantity').innerHTML = WeaponQuantity;
-		document.getElementById('WeaponTotalBonus').innerHTML = WeaponTotalBonus;
-		Damage++;		
-		UpdateStats();
-	}
-}
-
-function addDummy(){
-	if (EXP >= DummyCost){
-		EXP -= DummyCost;
-		DummyQuantity++;
-		DummyCost = Math.round(DummyCost * (1.15));
-		DummyTotalBonus+=5;
-		document.getElementById('DummyCost').innerHTML = DummyCost;
-		document.getElementById('DummyQuantity').innerHTML = DummyQuantity;
-		document.getElementById('DummyTotalBonus').innerHTML = DummyTotalBonus;
-		Damage += 5;
-		UpdateStats();
-	}
-}
-
-function addConcentration(){
-	if (EXP >= ConcentrationCost){
-		EXP -= ConcentrationCost;
-		ConcentrationQuantity++;
-		ConcentrationCost = Math.round(ConcentrationCost * (1.15));
-		ConcentrationTotalBonus += 10;
-		document.getElementById('ConcentrationCost').innerHTML = ConcentrationCost;
-		document.getElementById('ConcentrationQuantity').innerHTML = ConcentrationQuantity;
-		document.getElementById('ConcentrationTotalBonus').innerHTML = ConcentrationTotalBonus;
-		Damage += 10;
+		document.getElementById('WeaponDamage').innerHTML = WeaponDamage;
+		document.getElementById('WeaponLevel').innerHTML = WeaponLevel;
+		document.getElementById('WeaponName').innerHTML = WeaponName;
+		Damage = WeaponDamage;		
 		UpdateStats();
 	}
 }
 
 function addBeginner(){
-	if (EXP >= begCost){		
-		EXP -= Math.round(begCost * 100) / 100;
+	if (Currency >= begCost){		
+		Currency -= Math.round(begCost * 100) / 100;
 		begQuantity++;
 		begCost = Math.round(begCost * (1.15));
-		begTotalBonus++;
+		begTotalBonus+=begBonus;
 		document.getElementById('beginnerCost').innerHTML = begCost;
 		document.getElementById('beginnerQuantity').innerHTML = begQuantity;
 		document.getElementById('beginnerTotalBonus').innerHTML = begTotalBonus;
-		hitPsec++;
+		hitPsec+=begBonus;
 		UpdateStats();
 	}
 }
 
 function addAdvanced(){
-	if (EXP >= advCost){
-		EXP -= Math.round(advCost * 100) / 100;
+	if (Currency >= advCost){
+		Currency -= Math.round(advCost * 100) / 100;
 		advQuantity++;
 		advCost = Math.round(advCost * (1.15));
-		advTotalBonus+=5;
+		advTotalBonus+=advBonus;
 		document.getElementById('advancedCost').innerHTML = advCost;
 		document.getElementById('advancedQuantity').innerHTML = advQuantity;
 		document.getElementById('advancedTotalBonus').innerHTML = advTotalBonus;
-		hitPsec+= 5;
+		hitPsec+= advBonus;
 		UpdateStats();
 	}
 }
 
 function addSenior(){
-	if (EXP >= senCost){
-		EXP -= Math.round(senCost * 100) / 100;
+	if (Currency >= senCost){
+		Currency -= Math.round(senCost * 100) / 100;
 		senQuantity++;
 		senCost = Math.round(senCost * (1.15));
-		senTotalBonus+=20;
+		senTotalBonus+=senBonus;
 		document.getElementById('seniorCost').innerHTML = senCost;
 		document.getElementById('seniorQuantity').innerHTML = senQuantity;
 		document.getElementById('seniorTotalBonus').innerHTML = senTotalBonus;
-		hitPsec+= 20;
+		hitPsec+= senBonus;
+		UpdateStats();
+	}
+}
+
+function addGod(){
+	if (Currency >= godCost){
+		Currency -= Math.round(godCost * 100) / 100;
+		godQuantity++;
+		godCost = Math.round(godCost * (1.15));
+		godTotalBonus+=godBonus;
+		document.getElementById('godCost').innerHTML = godCost;
+		document.getElementById('godQuantity').innerHTML = godQuantity;
+		document.getElementById('godTotalBonus').innerHTML = godTotalBonus;
+		hitPsec+= godBonus;
 		UpdateStats();
 	}
 }
 
 function UpdateStats(){
-	expPsec = Math.round((TurnDamage / 10) * 100) / 100;
 	expPclick = Math.round((Damage / 10) * 100) / 100;
 	EXP = Math.round(EXP * 100) / 100;
 	document.getElementById('Damage').innerHTML = Damage;
-	document.getElementById("TurnDamage").innerHTML = TurnDamage;
-	document.getElementById('expPsec').innerHTML = expPsec;
+	document.getElementById("TurnDamage").innerHTML = hitPsec;
 	document.getElementById('EXP').innerHTML = EXP;
 	document.getElementById("expPclick").innerHTML = expPclick;
-	document.getElementById("lifeValue").innerHTML = life;
+	document.getElementById("lifeValue").innerHTML = Math.round(life * 100) / 100;
 	document.getElementById("totalLife").innerHTML = totalLife;
+	document.getElementById("gold").innerHTML = Currency;
 }
 
 function DummyMouseDown() {
@@ -164,26 +168,20 @@ function loseLife(dmg) {
 }
 
 function kill (){
-	EXP += Math.round(totalLife/10);
+	Currency += Math.round(totalLife/5);
 	totalLife = Math.round(totalLife * (1.05)); 
 	life = totalLife;
 	UpdateStats();
 }
 	
-function DPS(){
-	TurnDamage = Damage * hitPsec;
-	expPclick = TurnDamage / 10;
-	EXP += expPclick;
-	loseLife(TurnDamage);
-}
-	
 function startTimer(){
-	DPS();
+	var refreshDiv = refreshTime/10;
+	loseLife(hitPsec/refreshDiv)
 	document.getElementById("HitDamage").innerHTML = ".";
 	UpdateStats();
 setTimeout(function(){
 	startTimer();
-	}, 1000);
+	}, refreshTime);
 }
 	
 
